ec6037bb252c1efb3c3195ef564490cbe1fdf33c
thehimel <60639301+thehimel@users.noreply.github.com>
2023-10-03 20:36:30 +0200

    mocking_with_pytest: updated tests for advanced concept

--- mocking_with_pytest/advanced/tests/test_get_data.py
+++ mocking_with_pytest/advanced/tests/test_get_data.py
@@ -6,6 +6,37 @@ import pytest
 
 from mocking_with_pytest.advanced.get_data import get_countries_with_population
 
+mocked_response = {
+    "usa": {
+        "success": True,
+        "body": {
+            "name": "United States",
+            "currencies": ["USD"],
+            "region": "Americas",
+            "population": 329484123,
+        },
+    },
+    "canada": {
+        "success": True,
+        "body": {
+            "name": "Canada",
+            "currencies": ["CAD"],
+            "region": "Americas",
+            "population": 38005238,
+        },
+    },
+    "australia": {
+        "success": True,
+        "body": {
+            "name": "Australia",
+            "currencies": ["AUD"],
+            "region": "Oceania",
+            "population": 25687041,
+        },
+    },
+    "unknown": {"success": False, "body": {"status": 404, "message": "Not Found"}},
+}
+
 test_data = [
     (
         ["usa", "canada", "australia"],
@@ -26,36 +57,6 @@ test_data = [
 
 @pytest.mark.parametrize("country_names, expected", test_data)
 def test_get_countries_with_population(country_names, expected, mocker):
-    mocked_response = {
-        "usa": {
-            "success": True,
-            "body": {
-                "name": "United States",
-                "currencies": ["USD"],
-                "region": "Americas",
-                "population": 329484123,
-            },
-        },
-        "canada": {
-            "success": True,
-            "body": {
-                "name": "Canada",
-                "currencies": ["CAD"],
-                "region": "Americas",
-                "population": 38005238,
-            },
-        },
-        "australia": {
-            "success": True,
-            "body": {
-                "name": "Australia",
-                "currencies": ["AUD"],
-                "region": "Oceania",
-                "population": 25687041,
-            },
-        },
-        "unknown": {"success": False, "body": {"status": 404, "message": "Not Found"}},
-    }
     mocker.patch(
         target="mocking_with_pytest.advanced.get_data.get_country",
         side_effect=lambda *args, **kwargs: mocked_response[kwargs["country_name"]],
