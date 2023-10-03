c7e683aca448400cfc6c32da4b097fc98213abed
thehimel <60639301+thehimel@users.noreply.github.com>
2023-10-03 18:01:18 +0200

    Added fetch country data and init Country instance

---
+++ mocking_with_pytest/advanced/core.py
@@ -0,0 +1,26 @@
+"""Author: Himel Das"""
+
+from mocking_with_pytest.advanced.country import Country
+from mocking_with_pytest.simple.get_data import get_country
+from utilities.logger import logger
+
+
+def main():
+    name = "usa"
+    response = get_country(name=name)
+    data = response["body"]
+
+    if response["success"]:
+        country = Country(
+            name=data["name"],
+            currencies=data["currencies"],
+            region=data["region"],
+            population=data["population"],
+        )
+        logger.info(f"Country: {country.get_data()}")
+    else:
+        logger.error(msg=f"Error fetching data for the country '{name}': {data}.")
+
+
+if __name__ == "__main__":
+    main()
--- mocking_with_pytest/advanced/country.py
+++ mocking_with_pytest/advanced/country.py
@@ -1,37 +1,18 @@
 """Author: Himel Das"""
 
-import requests
-
-from utilities.logger import logger
-
 
 class Country:
-    def __init__(self, name: str):
+    def __init__(self, name: str, currencies: [str], region: str, population: int):
         """Initialise a Country object."""
-        self.name = None
-        self.currencies = None
-        self.region = None
-        self.population = None
-        self.set_country(name=name)
-        self.api_url = "https://restcountries.com/v3.1/name"
+        self.name = name
+        self.currencies = currencies
+        self.region = region
+        self.population = population
 
-    def set_country(self, name: str) -> dict or None:
-        response = requests.get(url=f"{self.api_url}/{name}")
-
-        if response.status_code == 200:
-            country_data = response.json()[0]
-            self.name = country_data["name"]["common"]
-            self.currencies = list(country_data["currencies"].keys())
-            self.region = country_data["region"]
-            self.population = country_data["population"]
-        else:
-            logger.error(msg=f"Error fetching data from the server. Status code: {response.status_code}")
-
-    def get_country(self):
-        data = {
+    def get_data(self):
+        return {
             "name": self.name,
             "currencies": self.currencies,
             "region": self.region,
             "population": self.region,
         }
-        return data
